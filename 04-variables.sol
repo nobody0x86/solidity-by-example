d1c4b6ac91299082571ca6c756d6d56f1ab342d8
nobody0x86 <nobody0x86@outlook.com>
2024-07-31 11:31:18 +0000

    chore: bump to a new version

--- 04-variables.sol
+++ 04-variables.sol
@@ -5,9 +5,9 @@ contract Variables {
     // State variables are stored on the blockchain
     string public greet = "Hello World!";
 
-    function doSomething() public view returns (int8, uint256, address) {
+    function doSomething() public view returns (int, uint256, address) {
         // Local variables are not saved to the blockchain
-        int8 i = 8;
+        int i = -256;
 
         // Global variables provide information about the blockchain
         uint256 timestamp = block.timestamp;
