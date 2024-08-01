d4721086f18104bc0102372dc38271615b89e634
nobody0x86 <nobody0x86@outlook.com>
2024-08-01 02:09:33 +0000

    chore: bump to a new version

---
+++ 10-if-else.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract Example {
+    function select(uint256 x) public pure returns (uint256) {
+        if (x < 10) {
+            return 0;
+        } else if (x < 20) {
+            return 1;
+        } else {
+            // ? is called the ternary operator
+            return x < 30 ? 2 : 3;
+        }
+    }
+}
