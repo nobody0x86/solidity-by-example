ef513933dcd8b84972a72ec82c05390d89d3e7e8
nobody0x86 <nobody0x86@outlook.com>
2024-08-05 02:48:05 +0000

    chore: bump to a new version

---
+++ 14-enum.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract Enum {
+    // Enum representing shipping status
+    enum Status {
+        Pending,
+        Shipped,
+        Accepted,
+        Rejected,
+        Canceled
+    }
+
+    // Default value is the first element listed in definition of the type
+    Status public status;
+
+    // Pending  - 0
+    // Shipped  - 1
+    // Accepted - 2
+    // Rejected - 3
+    // Canceled - 4
+    function get() public view returns (Status) {
+        return status;
+    }
+
+    // Update the status by passing uint into input
+    function set(Status _status) public {
+        status = _status;
+    }
+
+    // Update to a specific enum like this
+    function cancel() public {
+        status = Status.Canceled;
+    }
+
+    // Reset the enum to its first value, 0
+    function reset() public {
+        delete status;
+    }
+}
