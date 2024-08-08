1a67a4796023cc2ae3efa1914b5f2485d63106b4
nobody0x86 <nobody0x86@outlook.com>
2024-08-08 01:09:36 +0000

    chore: bump to a new version

---
+++ 18-view-and-pure-functions.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract Example {
+    uint256 public n = 1;
+
+    // Promise not to modify the state
+    function increase(uint256 count) public view returns (uint256) {
+        return count + n;
+    }
+
+    // Promise not to modify or read from the state
+    function add(uint256 x, uint256 y) public pure returns (uint256) {
+        return x + y;
+    }
+}
